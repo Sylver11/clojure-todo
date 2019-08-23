5017023c007b2b42de2094cbbe91bff46d097593
Sylver11 <justusvoigt@Justus-MacBook-Air.local>
2019-08-23 14:35:27 +0200

    x

--- src/routing/views/experience.clj
+++ src/routing/views/experience.clj
@@ -8,8 +8,8 @@
         )
  )
 
-
-
+;; Dummy variable
+;;(def req {"name" "justus"})
 
 
 
@@ -23,16 +23,35 @@
 
 
 
+;;This part should be capturing the parameter which have been inputed above.
+;;[req] should be the input
+;; the let function which in this case is a associative destructuring takes the key :params from req and from that takes the key :name to associate params with the name.
+;; But it throws me an error saying that it cannot resolve params in this context.
+;; However, according to documentation req should be a simple map {"name" "justus"} and it even says that in the url
+;;but I somehow struggle to the the destructuring right. I also dont really know how to debug this.
+;; I was thinking about using a dummy value for req which you can see on the top
+
 (defn display-result [req]
-  (let [{:keys [params]} req
-        param-name (get params "name")
+  (let [{{params :name} :params} req
+       ;; param-name (get params "name")
         ]
    (html
     [:div
-     [:h1 "Hello " (h param-name) "!"]
-     ])))
+     [:h1 "Hello " (h params) "!"]
+     ]))
+(println params)
+
+  )
 
 
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;this to be ingored
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; [:div {:class "form-group"}
 ;;             [:label {:for "formGroupExampleInput"} "First Name"]
